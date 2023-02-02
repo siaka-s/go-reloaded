dfa7005754f0ad3b1787021c5683c0f20e647bee
ssiahoue <siahouesiaka1@gmail.com>
2023-02-02 22:41:20 +0000

    test de la fonction convertirhex

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
+
+// Fonction hecadecimal en entier
+func hexconvert() {
+
+	// initialisation d'une variable  pour effectuer le test
+	hexadecimal_num := "23a"
+
+	// la fonction parseInt() nous permet de réaliser la conversion
+	decimal_num, err := strconv.ParseInt(hexadecimal_num, 16, 64)
+
+	// Gestion de l'erreur
+	if err != nil {
+		panic(err)
+	}
+
+	// Affichage du résultat
+	fmt.Println(decimal_num)
+
 }
