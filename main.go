47d8bc5db5c9f86308b9d905cffaea91840f3f5d
ssiahoue <siahouesiaka1@gmail.com>
2023-02-06 17:54:31 +0000

    convertion en hexadécimal et affichage des modification dan result.txt

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Fonction hecadecimal en entier
-func convertirhex(hexnum string) {
+func convertirhex(hexnum string) int64 {
 
 	// la fonction parseInt() nous permet de réaliser la conversion
 	decimal_num, err := strconv.ParseInt(hexnum, 16, 64)
@@ -18,10 +18,7 @@ func convertirhex(hexnum string) {
 	if err != nil {
 		panic(err)
 	}
-
-	// Affichage du résultat
-	fmt.Println(decimal_num)
-
+	return decimal_num
 }
 
 // stocage de la phrase dans un tableau et execution de cas
@@ -29,40 +26,65 @@ func convertirhex(hexnum string) {
 func wordTabExe(sample string) []byte {
 
 	// récuperation du contenu du filchier
-	phrase, err := ioutil.ReadFile(sample)
+	mots, err := ioutil.ReadFile(sample)
 
 	if err != nil {
 		fmt.Println("Erreur lors de la lecture du fichier:", err)
 	}
-	return phrase
+	return mots
 }
 
-func traitement(phrase string) {
+func traitement(phrase string) string {
 
 	mots := strings.Fields(phrase)
 
-	fmt.Println(mots[1])
+	for i := 0; i < len(mots); i++ {
 
-	for _, v := range mots {
-		if v == "(hex)" {
-			convertirhex(mots[0])
+		if mots[i] == "(hex)" {
+
+			mots[i] = ""
+
+			a := i - 1
+
+			dec := convertirhex(mots[a])
+
+			decf := strconv.FormatInt(dec, 10)
+
+			// assigné la valeur a la position
+			mots[a] = decf
+			// convertir le chiffre dec en chaine de caractère
 		}
 	}
+	// convertir le tableau en chaine de caractère
+	result := strings.Join(mots, " ")
+	// enlever les espaces après traitement
+	result = strings.Replace(result, "  ", " ", -1)
 
+	return result
+}
+
+func remplacerContenuFichier(nomFichier string, contenu string) error {
+	return ioutil.WriteFile(nomFichier, []byte(contenu), 0666)
 }
 
 func main() {
 
 	sample := os.Args[1]
 
-	// result := os.Args[2]
+	result := os.Args[2]
 
 	// strockage du contenu du fichier dans une variable
 	phrase := string(wordTabExe(sample))
 
 	// exécution de la fonction de traitement
-	traitement(phrase)
+	contenu := (traitement(phrase))
 
-	// fmt.Printf("le texte de debut est %v \n le texte modifier est %v", sample, result)
+	// execution de la fontion remplacement avec affichage de message d'exécution réussie ou d'erreur
+	err := remplacerContenuFichier(result, contenu)
+	if err != nil {
+		fmt.Println("Erreur lors de la réécriture du fichier :", err)
+	} else {
+		fmt.Println("Le contenu du fichier a été remplacé avec succès")
+	}
 
 }
