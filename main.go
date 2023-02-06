839b1f41fba0662bff27595bd30d07ce8d51c354
ssiahoue <siahouesiaka1@gmail.com>
2023-02-06 18:45:04 +0000

    Ajout de la fonction de la convertion binaire en décimal

--- main.go
+++ main.go
@@ -8,20 +8,28 @@ import (
 	"strings"
 )
 
-// Fonction hecadecimal en entier
+// Fonction de convertion hecadecimal en entier
 func convertirhex(hexnum string) int64 {
 
-	// la fonction parseInt() nous permet de réaliser la conversion
 	decimal_num, err := strconv.ParseInt(hexnum, 16, 64)
 
-	// Gestion de l'erreur du nombre recu en paramètre
 	if err != nil {
 		panic(err)
 	}
 	return decimal_num
 }
 
-// stocage de la phrase dans un tableau et execution de cas
+// convertion de binaire en entier
+func convertirbin(binaire string) int64 {
+
+	n, err := strconv.ParseInt(binaire, 2, 64)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	return n
+}
+
+// stockage de la phrase dans un tableau et execution de cas
 
 func wordTabExe(sample string) []byte {
 
@@ -34,28 +42,48 @@ func wordTabExe(sample string) []byte {
 	return mots
 }
 
+// Fonction de traitement
 func traitement(phrase string) string {
 
 	mots := strings.Fields(phrase)
 
 	for i := 0; i < len(mots); i++ {
 
-		if mots[i] == "(hex)" {
-
+		switch mots[i] {
+		case "(hex)":
 			mots[i] = ""
 
 			a := i - 1
 
 			dec := convertirhex(mots[a])
-
+			// converti ne nombre en chaine de caratère
 			decf := strconv.FormatInt(dec, 10)
 
 			// assigné la valeur a la position
 			mots[a] = decf
 			// convertir le chiffre dec en chaine de caractère
+		case "(bin)":
+			mots[i] = ""
+
+			a := i - 1
+
+			dec := convertirbin(mots[a])
+
+			decf := strconv.FormatInt(dec, 10)
+
+			mots[a] = decf
+
+		case "(up)":
+
+		case "(low)":
+
+		case "(cap)":
+
+		default:
+			fmt.Println("Aucune modification à éffectuer")
 		}
 	}
-	// convertir le tableau en chaine de caractère
+	// convertir le tableau en chaine de caractère avec un espace de séparation
 	result := strings.Join(mots, " ")
 	// enlever les espaces après traitement
 	result = strings.Replace(result, "  ", " ", -1)
@@ -63,8 +91,9 @@ func traitement(phrase string) string {
 	return result
 }
 
-func remplacerContenuFichier(nomFichier string, contenu string) error {
-	return ioutil.WriteFile(nomFichier, []byte(contenu), 0666)
+// Migration du contenu modifier dans un nouveau fichier
+func remplacerContenuFichier(nvFichier string, newContenu string) error {
+	return ioutil.WriteFile(nvFichier, []byte(newContenu), 0666)
 }
 
 func main() {
